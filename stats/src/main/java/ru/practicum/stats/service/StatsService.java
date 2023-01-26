4e2c48b26aa3fdd13701dd36366fddc1234d744c
Ruslan8999 <rponomarenko89@yandex.ru>
2023-01-26 22:46:12 +0300

    feat: stat_svc

--- stats/src/main/java/ru/practicum/stats/service/StatsService.java
+++ stats/src/main/java/ru/practicum/stats/service/StatsService.java
@@ -7,5 +7,6 @@ import java.util.List;
 
 public interface StatsService {
     void saveHit(StatInDto statInDto);
+
     List<StatOutDto> getHit(String start, String end, List<String> uris, Boolean unique);
 }
