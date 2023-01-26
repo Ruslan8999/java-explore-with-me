96f7f31ae992cf5c827762a656a554d543b2f2b8
Ruslan8999 <rponomarenko89@yandex.ru>
2023-01-26 23:27:24 +0300

    feat: stat_svc

--- stats/src/main/java/ru/practicum/stats/repository/StatsRepository.java
+++ stats/src/main/java/ru/practicum/stats/repository/StatsRepository.java
@@ -18,7 +18,8 @@ public interface StatsRepository extends JpaRepository<Stat, Long> {
     @Query(value = "SELECT new ru.practicum.stats.dto.StatOutDto(s.app, s.uri, COUNT (s.id)) " +
             "FROM Stat s " +
             "WHERE s.uri IN :uris AND s.timestamp BETWEEN :start AND :end " +
-            "GROUP BY s.app, s.uri")
+            "GROUP BY s.app, s.uri " +
+            "ORDER BY COUNT (s.id) DESC")
     List<StatOutDto> countByTimestampAndList(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query(value = "SELECT new ru.practicum.stats.dto.StatOutDto(s.app, s.uri, COUNT (s.id)) " +
