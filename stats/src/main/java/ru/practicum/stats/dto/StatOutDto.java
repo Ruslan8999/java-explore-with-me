11e04f66df3299a4e649836fd4025185cf11849d
Ruslan8999 <rponomarenko89@yandex.ru>
2023-02-02 13:18:15 +0300

    feat: stat_svc

--- stats/src/main/java/ru/practicum/stats/dto/StatOutDto.java
+++ stats/src/main/java/ru/practicum/stats/dto/StatOutDto.java
@@ -5,11 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.Min;
+
 @Data
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StatOutDto {
     String app;
     String uri;
+    @Min(0)
     Long hits;
 }
